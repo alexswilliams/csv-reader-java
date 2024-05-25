2c0648b651e8e0c214724a6d7ae65ce00a0bab4f
Alex Williams <lexw@live.co.uk>
2024-05-25 20:46:00 +0100

    Find more bugs with more tests

--- lib/src/test/java/io/github/alexswilliams/csv/CsvReaderUnitTest.java
+++ lib/src/test/java/io/github/alexswilliams/csv/CsvReaderUnitTest.java
@@ -112,7 +112,7 @@ public class CsvReaderUnitTest {
                 new Assertion("Quoted values starting with escaped quotes are unescaped", "\"\"\" B\"", list(list("\" B"))),
                 new Assertion("Quoted values ending with escaped quotes are unescaped", "\"A \"\"\"", list(list("A \""))),
                 new Assertion("A value can contain quoted text", "\"Bob \"\"The Cheese\"\" Windsor\"", list(list("Bob \"The Cheese\" Windsor"))),
-                new Assertion("A value can be just one escaped quote", "\"\"\"\"", list(list("\"")))
+                new Assertion("A value can be just one escaped quote", "\"\"\"\"", list(list("\""))),
                 // bug: new Assertion("A value can be just two escaped quotes", "\"\"\"\"\"\"", list(list("\"\""))),
                 // bug: new Assertion("Quotes values that wrap their values in quotes are unescaped", "\"\"\"Cell\"\"\"", list(list("\"Cell\""))),
 
@@ -129,31 +129,40 @@ public class CsvReaderUnitTest {
                 new Assertion("LF can start the field", "\"\r\nB\"", list(list("\r\nB"))),
                 new Assertion("LF can end the field", "\"A\r\n\"", list(list("A\r\n"))), */
 
-        ).map(it -> DynamicTest.dynamicTest(it.displayName, () -> {
+                // Comments
+                new Assertion("The whole document can be a comment", "#", list()),
+                new Assertion("The whole document can be a comment (plus newline)", "#\n", list()),
+                new Assertion("Commas after comment do not create new fields (doc start)", "#, a, b", list()),
+                new Assertion("Commas after comment do not create new fields (line start)", "cell 1\n#, a, b", list(list("cell 1"))),
+                new Assertion("Whitespace before comment is significant", "cell 1 # a, b", list(list("cell 1 "))),
+                new Assertion("Whitespace before comment and after comma creates a blank field", "cell 1, # a, b", list(list("cell 1", " "))),
+                // bug: new Assertion("Quoted values can contain comment character at start", "\"# cell\"", list(list("# cell"))),
+                // bug: new Assertion("Quoted values can contain comment character at end", "\"cell #\"", list(list("cell #"))),
+                // bug: new Assertion("Quoted values can contain comment character in middle", "\"cell # cell\"", list(list("cell # cell"))),
+                // bug: new Assertion("Comment character can start the next line of a multiline quoted value", "\"line 1\n# line 2\"", list(list("line 1\n# line 2"))),
+                // bug: new Assertion("Comment character cab appear on the next line of a multiline quoted value", "\"line 1\nline 2 #\"", list(list("line 1\nline 2 #"))),
+
+                null
+        ).filter(Objects::nonNull).map(it -> DynamicTest.dynamicTest(it.displayName, () -> {
             final List<List<String>> output;
             try (StringReader stringReader = new StringReader(it.input); CsvReader csvReader = new CsvReader(stringReader)) {
                 output = csvReader.readFile(false);
             }
-            Assertions.assertIterableEquals(it.expectedOutput, output);
+            Assertions.assertEquals(it.expectedOutput, output);
         }));
     }
 
     @Test
     public void testReadFile() throws IOException, CsvException {
         final StringBuilder stringBuilder = initStrings();
-
         try (final CsvReader csvReader = new CsvReader(new StringReader(stringBuilder.toString()))) {
-
             final List<List<String>> result = csvReader.readFile(true);
 
             Assertions.assertNotNull(result, "ListList was null");
             Assertions.assertEquals(result.size(), 19, "Wrong number of elements");
-            Assertions.assertEquals(result.get(2), Collections.emptyList(),
-                    "'\\n' isn't represented as an empty list.");
-            Assertions.assertEquals(result.stream().filter(Objects::isNull).count(), 0,
-                    "Some results were null.");
-            Assertions.assertEquals(result.stream().mapToLong(Collection::size).sum(), 64,
-                    "Wrong number of total fields returned.");
+            Assertions.assertEquals(result.get(2), Collections.emptyList(), "'\\n' isn't represented as an empty list.");
+            Assertions.assertEquals(result.stream().filter(Objects::isNull).count(), 0, "Some results were null.");
+            Assertions.assertEquals(result.stream().mapToLong(Collection::size).sum(), 64, "Wrong number of total fields returned.");
         }
     }
 
@@ -176,8 +185,7 @@ public class CsvReaderUnitTest {
         stringBuilder.append(StringEscapeUtils.escapeCsv("tricky,input")).append(",quotes\n");
         stringBuilder.append(StringEscapeUtils.escapeCsv("tricky\"\"input")).append(",quotes\n");
         stringBuilder.append(StringEscapeUtils.escapeCsv("tricky\",\"input")).append(",quotes\n");
-        stringBuilder.append(StringEscapeUtils.escapeCsv("\"aꜬbꜢd\uD801\uDF22e∛f\""))
-                .append(",quotes\n");
+        stringBuilder.append(StringEscapeUtils.escapeCsv("\"aꜬbꜢd\uD801\uDF22e∛f\"")).append(",quotes\n");
         stringBuilder.append("# comment line, with \" many # odd ,\"'\"\" characters in.\n");
         stringBuilder.append("# comment line, with \" many # odd ,\"'\"\" characters in.\n");
         stringBuilder.append("\"\"\"\"\n");
@@ -246,77 +254,65 @@ public class CsvReaderUnitTest {
             Assertions.assertNotNull(line9, "List was null");
             Assertions.assertEquals(line9.size(), 2, "Wrong number of elements");
             Assertions.assertNotNull(line9.get(0), "String was null");
-            Assertions.assertEquals(line9.get(0), "tricky'input",
-                    "Apostrophe (') character was mis-interpreted.");
+            Assertions.assertEquals(line9.get(0), "tricky'input", "Apostrophe (') character was mis-interpreted.");
 
             final List<String> line10 = csvReader.parseLine();
             Assertions.assertNotNull(line10, "List was null");
             Assertions.assertEquals(line10.size(), 2, "Wrong number of elements");
             Assertions.assertNotNull(line10.get(0), "String was null");
-            Assertions.assertEquals(line10.get(0), "tricky\"input",
-                    "Interstitial quotation mark (\") was mis-interpreted.");
+            Assertions.assertEquals(line10.get(0), "tricky\"input", "Interstitial quotation mark (\") was mis-interpreted.");
 
             final List<String> line11 = csvReader.parseLine();
             Assertions.assertNotNull(line11, "List was null");
             Assertions.assertEquals(line11.size(), 2, "Wrong number of elements");
             Assertions.assertNotNull(line11.get(0), "String was null");
-            Assertions.assertEquals(line11.get(0), "tricky,input",
-                    "Interstitial comma (,) was mis-interpreted.");
+            Assertions.assertEquals(line11.get(0), "tricky,input", "Interstitial comma (,) was mis-interpreted.");
 
             final List<String> line12 = csvReader.parseLine();
             Assertions.assertNotNull(line12, "List was null");
             Assertions.assertEquals(line12.size(), 2, "Wrong number of elements");
             Assertions.assertNotNull(line12.get(0), "String was null");
-            Assertions.assertEquals(line12.get(0), "tricky\"\"input",
-                    "Pair of quotation marks (\"\") was mis-interpreted.");
+            Assertions.assertEquals(line12.get(0), "tricky\"\"input", "Pair of quotation marks (\"\") was mis-interpreted.");
 
             final List<String> line13 = csvReader.parseLine();
             Assertions.assertNotNull(line13, "List was null");
             Assertions.assertEquals(line13.size(), 2, "Wrong number of elements");
             Assertions.assertNotNull(line13.get(0), "String was null");
-            Assertions.assertEquals(line13.get(0), "tricky\",\"input",
-                    "Sequence \",\" was mis-interpreted.");
+            Assertions.assertEquals(line13.get(0), "tricky\",\"input", "Sequence \",\" was mis-interpreted.");
 
             final List<String> line14 = csvReader.parseLine();
             Assertions.assertNotNull(line14, "List was null");
             Assertions.assertEquals(line14.size(), 2, "Wrong number of elements");
             Assertions.assertNotNull(line14.get(0), "String was null");
-            Assertions.assertEquals(line14.get(0), "aꜬbꜢd\uD801\uDF22e∛f",
-                    "Quoted unicode characters caused problems.");
+            Assertions.assertEquals(line14.get(0), "aꜬbꜢd\uD801\uDF22e∛f", "Quoted unicode characters caused problems.");
 
             final List<String> line15 = csvReader.parseLine();
             Assertions.assertNotNull(line15, "List was null");
             Assertions.assertEquals(line15.size(), 1, "Wrong number of elements");
             Assertions.assertNotNull(line15.get(0), "String was null");
-            Assertions.assertEquals(line15.get(0), "\"",
-                    "The raw field \"\"\"\" didn't get unescaped to a single \" character.");
+            Assertions.assertEquals(line15.get(0), "\"", "The raw field \"\"\"\" didn't get unescaped to a single \" character.");
 
             final List<String> line16 = csvReader.parseLine();
             Assertions.assertNotNull(line16, "List was null");
             Assertions.assertEquals(line16.size(), 1, "Wrong number of elements");
             Assertions.assertNotNull(line16.get(0), "String was null");
-            Assertions.assertEquals(line16.get(0), "bob \"wonderful\" smith",
-                    "An interstitially quoted string was mis-interpreted.");
+            Assertions.assertEquals(line16.get(0), "bob \"wonderful\" smith", "An interstitially quoted string was mis-interpreted.");
 
             final List<String> line17 = csvReader.parseLine();
             Assertions.assertNotNull(line17, "List was null");
             Assertions.assertEquals(line17.size(), 4, "Wrong number of elements");
             Assertions.assertNotNull(line17.get(0), "String was null");
-            Assertions.assertEquals(line17.get(0), "0",
-                    "Normal single-character string was not pulled out successfully.");
+            Assertions.assertEquals(line17.get(0), "0", "Normal single-character string was not pulled out successfully.");
             Assertions.assertNotNull(line17.get(3), "String with many quotations was null.");
-            Assertions.assertEquals(line17.get(3), "\"AAAAAAAAAA\": BB BB BB 'ABCDEFG QWEIR', 1234-5678",
-                    "Interstitial quotation marks at the beginning of the string caused an issue.");
+            Assertions.assertEquals(line17.get(3), "\"AAAAAAAAAA\": BB BB BB 'ABCDEFG QWEIR', 1234-5678", "Interstitial quotation marks at the beginning of the string caused an issue.");
 
             final List<String> line18 = csvReader.parseLine();
             Assertions.assertNotNull(line18, "List was null");
             Assertions.assertEquals(line18.size(), 28, "Wrong number of elements");
             Assertions.assertNotNull(line18.get(0), "String was null");
-            Assertions.assertEquals(line18.get(0), "333333333",
-                    "Made-up stucode was not pulled out successfully.");
+            Assertions.assertEquals(line18.get(0), "333333333", "Made-up stucode was not pulled out successfully.");
             Assertions.assertNotNull(line18.get(27), "String with many quotations was null.");
-            Assertions.assertEquals(line18.get(27), "\"NNNNNNNNNNNN AMBNDJ\": AMAMAMMAMAMA 'QPQPQPQPQP', 1234-5678",
-                    "Interstitial quotation marks at the beginning of the string caused an issue.");
+            Assertions.assertEquals(line18.get(27), "\"NNNNNNNNNNNN AMBNDJ\": AMAMAMMAMAMA 'QPQPQPQPQP', 1234-5678", "Interstitial quotation marks at the beginning of the string caused an issue.");
 
             final List<String> line19 = csvReader.parseLine();
             Assertions.assertNotNull(line19, "List was null");
